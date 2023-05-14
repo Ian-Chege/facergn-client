9112053d029690b15701bc811d4f88ff9a5c170a
Ian Chege <ianchege001@gmail.com>
2023-05-14 20:27:27 +0300

    updated fetch urls

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://facergn-server-v7zh.vercel.app/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -80,7 +80,7 @@ class App extends Component {
     .then(response => response.json())
       
       .then(response => {
-          fetch('http://localhost:3000/image', {
+          fetch('https://facergn-server-v7zh.vercel.app/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
