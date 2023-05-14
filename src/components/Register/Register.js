5944f14dac73f390023c78f56768f9086232f987
Ian Chege <ianchege001@gmail.com>
2023-05-14 20:55:14 +0300

    Updated url

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://facergn-server-v7zh.vercel.app/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
